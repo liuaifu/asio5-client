5ffbe58cc9dbc5c600375d6b4dc122e8f49a5e47
liuaifu <liuaifu@migu.cn>
2018-08-31 15:14:19 +0800

    使用boost.fiber重构

--- session.cpp
+++ session.cpp
@@ -4,11 +4,9 @@
 #include <iostream>
 
 extern boost::asio::io_service g_io_service;
-session::session(boost::asio::io_service& _io_service)
-	:client_socket(_io_service), server_socket(_io_service), strand_(_io_service)
+session::session(boost::shared_ptr<boost::asio::io_service> io_svc, boost::shared_ptr<tcp::socket> sck)
+	:client_socket(sck), server_socket(*io_svc)
 {
-	client_recv_count = 0;
-	stopping = false;
 }
 
 session::~session(void)
@@ -16,14 +14,25 @@ session::~session(void)
 
 }
 
-void session::start()
+void session::run()
 {
 	BOOST_LOG_TRIVIAL(debug) << "session start";
-	server_socket.async_connect(*g_proxy_iterator,
-		strand_.wrap(boost::bind(&session::handle_connect_server, shared_from_this(),
-			boost::asio::placeholders::error, g_proxy_iterator)
-		)
+	boost::system::error_code ec;
+	server_socket.async_connect(
+		*g_proxy_iterator,
+		boost::fibers::asio::yield[ec]
 	);
+
+	if (ec) {
+		BOOST_LOG_TRIVIAL(error) << "open " << g_proxy_iterator->host_name() << " fail, " << ec.message();
+		stop();
+		return;
+	}
+
+	BOOST_LOG_TRIVIAL(debug) << "open " << g_proxy_iterator->host_name();
+
+	boost::fibers::fiber(boost::bind(&session::read_server_loop, shared_from_this())).detach();
+	read_client_loop();
 }
 
 void session::stop()
@@ -36,7 +45,7 @@ void session::stop()
 	}
 	
 	try {
-		client_socket.close();
+		client_socket->close();
 	}
 	catch (const std::exception&) {
 
@@ -45,137 +54,86 @@ void session::stop()
 	BOOST_LOG_TRIVIAL(debug) << "session stopped";
 }
 
-void session::write_to_client(char *data, size_t size)
+void session::read_server_loop()
+{
+	char server_buf[SOCKET_RECV_BUF_LEN];
+	boost::system::error_code ec;
+
+	while (true) {
+		size_t bytes_transferred = server_socket.async_read_some(
+			boost::asio::buffer(server_buf, SOCKET_RECV_BUF_LEN),
+			boost::fibers::asio::yield[ec]
+		);
+		if (ec) {
+			if (ec == boost::asio::error::operation_aborted)
+				return;
+			BOOST_LOG_TRIVIAL(error) << "read from server fail, " << ec.message();
+			stop();
+			return;
+		}
+		write_to_client(server_buf, bytes_transferred, ec);
+		if (ec) {
+			if (ec == boost::asio::error::operation_aborted)
+				return;
+			BOOST_LOG_TRIVIAL(error) << "write to client fail, " << ec.message();
+			stop();
+			return;
+		}
+	}
+}
+
+void session::read_client_loop()
+{
+	char client_buf[SOCKET_RECV_BUF_LEN];
+	boost::system::error_code ec;
+
+	while (true) {
+		size_t bytes_transferred = client_socket->async_read_some(
+			boost::asio::buffer(client_buf, SOCKET_RECV_BUF_LEN),
+			boost::fibers::asio::yield[ec]
+		);
+		if (ec) {
+			if (ec == boost::asio::error::operation_aborted)
+				return;
+			BOOST_LOG_TRIVIAL(error) << "read from client fail, " << ec.message();
+			stop();
+			return;
+		}
+		write_to_server(client_buf, bytes_transferred, ec);
+		if (ec) {
+			if (ec == boost::asio::error::operation_aborted)
+				return;
+			BOOST_LOG_TRIVIAL(error) << "write to server fail, " << ec.message();
+			stop();
+			return;
+		}
+	}
+}
+
+void session::write_to_client(char *data, size_t size, boost::system::error_code& ec)
 {
 	boost::shared_ptr<char> data_ptr = boost::shared_ptr<char>(new char[size]);
 	memcpy(data_ptr.get(), data, size);
 	for (size_t i = 0; i < size; i++)
 		data_ptr.get()[i] ^= 'F';
 
-	boost::asio::async_write(client_socket,
+	boost::asio::async_write(
+		*client_socket,
 		boost::asio::buffer(data_ptr.get(), size),
-		strand_.wrap(boost::bind(&session::handle_client_write, shared_from_this(),
-			data_ptr,
-			boost::asio::placeholders::error)
-		)
+		boost::fibers::asio::yield[ec]
 	);
 }
 
-void session::handle_resolve(const boost::system::error_code& error, boost::asio::ip::tcp::resolver::results_type results)
-{
-
-}
-
-void session::handle_client_read(const boost::system::error_code& error, size_t bytes_transferred)
-{
-	if (error) {
-		if (error == boost::asio::error::operation_aborted)
-			return;
-
-		BOOST_LOG_TRIVIAL(error) << error;
-		stop();
-		return;
-	}
-
-	write_to_server(client_buf, bytes_transferred);
-
-	client_socket.async_read_some(boost::asio::buffer(client_buf, SOCKET_RECV_BUF_LEN),
-		strand_.wrap(boost::bind(&session::handle_client_read, shared_from_this(),
-			boost::asio::placeholders::error,
-			boost::asio::placeholders::bytes_transferred)
-		)
-	);
-}
-
-void session::handle_client_write(boost::shared_ptr<char> data, const boost::system::error_code& error)
-{
-	if (error)
-	{
-		if (error == boost::asio::error::operation_aborted)
-			return;
-
-		BOOST_LOG_TRIVIAL(error) << error;
-		stop();
-		return;
-	}
-}
-
-void session::handle_connect_server(const boost::system::error_code& error, tcp::resolver::iterator endpoint_iterator)
-{
-	if (error)
-	{
-		if (error == boost::asio::error::operation_aborted)
-			return;
-
-		BOOST_LOG_TRIVIAL(error) << "open " << endpoint_iterator->host_name() << " fail, " << error;
-		stop();
-		return;
-	}
-
-	BOOST_LOG_TRIVIAL(debug) << "open " << endpoint_iterator->host_name();
-
-	server_socket.async_read_some(boost::asio::buffer(server_buf, SOCKET_RECV_BUF_LEN),
-		strand_.wrap(boost::bind(&session::handle_server_read, shared_from_this(),
-			boost::asio::placeholders::error,
-			boost::asio::placeholders::bytes_transferred)
-		)
-	);
-
-	client_socket.async_read_some(boost::asio::buffer(client_buf, SOCKET_RECV_BUF_LEN),
-		strand_.wrap(boost::bind(&session::handle_client_read, shared_from_this(),
-			boost::asio::placeholders::error,
-			boost::asio::placeholders::bytes_transferred)
-		)
-	);
-}
-
-void session::write_to_server(char *data, size_t size)
+void session::write_to_server(char *data, size_t size, boost::system::error_code& ec)
 {
 	auto data_ptr = boost::shared_ptr<char>(new char[size]);
 	memcpy(data_ptr.get(), data, size);
 	for (size_t i = 0; i < size; i++)
 		data_ptr.get()[i] ^= 'F';
 
-	boost::asio::async_write(server_socket,
+	boost::asio::async_write(
+		server_socket,
 		boost::asio::buffer(data_ptr.get(), size),
-		strand_.wrap(boost::bind(&session::handle_server_write, shared_from_this(),
-			data_ptr, boost::asio::placeholders::error)
-		)
+		boost::fibers::asio::yield[ec]
 	);
-}
-
-void session::handle_server_read(const boost::system::error_code& error, size_t bytes_transferred)
-{
-	if (error) {
-		if (error == boost::asio::error::operation_aborted)
-			return;
-		if (error == boost::asio::error::eof) {
-			stopping = true;
-			return;
-		}
-		BOOST_LOG_TRIVIAL(error) << error;
-		stop();
-		return;
-	}
-
-	write_to_client(server_buf, bytes_transferred);
-	server_socket.async_read_some(boost::asio::buffer(server_buf, SOCKET_RECV_BUF_LEN),
-		strand_.wrap(boost::bind(&session::handle_server_read, shared_from_this(),
-			boost::asio::placeholders::error,
-			boost::asio::placeholders::bytes_transferred)
-		)
-	);
-}
-
-void session::handle_server_write(boost::shared_ptr<char> data_ptr, const boost::system::error_code& error)
-{
-	if (error)
-	{
-		if (error == boost::asio::error::operation_aborted)
-			return;
-
-		BOOST_LOG_TRIVIAL(error) << error;
-		stop();
-		return;
-	}
 }
--- session.h
+++ session.h
@@ -1,44 +1,27 @@
 ﻿#pragma once
 #include <boost/shared_ptr.hpp>
 #include <boost/enable_shared_from_this.hpp>
-#include <boost/thread/mutex.hpp>
 #include <boost/asio.hpp>
-#include <map>
 
-using namespace std;
 using boost::asio::ip::tcp;
 
 class session : public boost::enable_shared_from_this<session>
 {
 public:
-	session(boost::asio::io_service& _io_service);
+	session(boost::shared_ptr<boost::asio::io_service> io_svc, boost::shared_ptr<tcp::socket> sck);
 	~session(void);
 
-	void start();
+	void run();
+	void read_server_loop();
+	void read_client_loop();
+	void write_to_client(char *data, size_t size, boost::system::error_code& ec);
+	void write_to_server(char *data, size_t size, boost::system::error_code& ec);
+
+private:
 	void stop();
-	void write_to_client(char *data, size_t size);
-	void handle_client_read(const boost::system::error_code& error, size_t bytes_transferred);
-	void handle_client_write(boost::shared_ptr<char> data, const boost::system::error_code& error);
-	void handle_resolve(const boost::system::error_code& error, boost::asio::ip::tcp::resolver::results_type results);
-	void handle_connect_server(const boost::system::error_code& error, tcp::resolver::iterator endpoint_iterator);
-
-	void write_to_server(char *data, size_t size);
-	void handle_server_read(const boost::system::error_code& error, size_t bytes_transferred);
-	void handle_server_write(boost::shared_ptr<char> data_ptr, const boost::system::error_code& error);
-
-public:
-	boost::mutex mutex_;
-	boost::asio::io_service::strand strand_;
-	tcp::socket client_socket;
+	boost::shared_ptr<tcp::socket> client_socket;
 
 private:
 	enum { SOCKET_RECV_BUF_LEN = 1024 };
-
 	tcp::socket server_socket;
-
-	char client_buf[SOCKET_RECV_BUF_LEN];
-	char server_buf[SOCKET_RECV_BUF_LEN];
-
-	int client_recv_count;
-	bool stopping;
 };
